5c4127d3a0b3222951bfad189e8a12f8e32a19f1
Oscar6 <oscar.m1024@gmail.com>
2018-11-15 16:24:37 -0600

    complete bonus step

---
+++ guessExercise.py
@@ -0,0 +1,66 @@
+# Step 1
+# guessNumber = 5
+# guess = int(input("Guess a number between 1 - 5: "))
+# while guess != guessNumber:
+#     print("Guess again")
+#     guess = int(input("Guess a number between 1 - 5: "))
+# if guess == guessNumber:
+#     print("You guessed correctly")
+
+# Step 2
+# guessNumber = 5
+# guess = int(input("Guess a number between 1 - 5: "))
+# while guess != guessNumber:
+#     if guess > guessNumber:
+#         print(guess, "is too high. Guess again")
+#         guess = int(input("Guess a number between 1 - 5: "))
+        
+#     elif guess < guessNumber:
+#         print(guess, "is too low. Guess again")
+#         guess = int(input("Guess a number between 1 - 5: "))
+
+
+# if guess == guessNumber:
+#     print("You guessed correctly")
+
+# Step 3
+# import random 
+# ranNum = random.randint(1, 10)
+
+# guess = int(input("Guess a number between 1 - 5: "))
+# while guess != ranNum:
+#     if guess > ranNum:
+#         print(guess, "is too high. Guess again")
+#         guess = int(input("Guess a number between 1 - 5: "))
+        
+#     elif guess < ranNum:
+#         print(guess, "is too low. Guess again")
+#         guess = int(input("Guess a number between 1 - 5: "))
+
+
+# if guess == ranNum:
+#     print("You guessed correctly")
+
+# Step 4
+import random 
+ranNum = random.randint(1, 10)
+
+count = 0
+guess = int(input("Guess a number between 1 - 5: "))
+while guess != ranNum:
+    count += 1
+    if count == 5:
+        print("You are out of guesses.")
+        break
+
+    elif guess > ranNum:
+        print(guess, "is too high. Guess again")
+        guess = int(input("Guess a number between 1 - 5: "))
+        
+    elif guess < ranNum:
+        print(guess, "is too low. Guess again")
+        guess = int(input("Guess a number between 1 - 5: "))
+    
+
+if guess == ranNum:
+    print("You guessed correctly")
