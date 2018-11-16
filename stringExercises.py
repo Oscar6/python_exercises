33b823bc9433682c28b3735076da513cc9e8d4aa
Oscar6 <oscar.m1024@gmail.com>
2018-11-16 14:02:37 -0600

    work on reverse

---
+++ stringExercises.py
@@ -0,0 +1,51 @@
+# 1. Uppercase a String
+# food = 'pizza'
+# print(food.upper())
+
+# 2. Capitalize a String
+# food = 'pizza'
+# print(food.capitalize())
+
+# 3. Reverse a String
+# userInput = str(input('Enter text: '))
+# string = userInput[::-1]
+
+# print(string) 
+
+######## OR ########
+
+# textUser = input('Enter text: ')
+# arrayLength = len(textUser)
+# newText = ""
+# for i in textUser:
+#     newText += textUser[arrayLength - 1 - i]
+# print(newText)
+
+# 4. Leetspeak
+# paragraph = input('Enter some text: ').upper()
+
+# paragraph = paragraph.replace('A', '4')
+# paragraph = paragraph.replace('E', '3')
+# paragraph = paragraph.replace('G', '6')
+# paragraph = paragraph.replace('I', '1')
+# paragraph = paragraph.replace('O', '0')
+# paragraph = paragraph.replace('S', '5')
+# paragraph = paragraph.replace('T', '7')
+
+# print(paragraph)
+
+# 5. Long-long Vowels
+# word = input('Enter a long vowel word: ')
+# word = word.replace('aa', 'aaaaa')
+# word = word.replace('ee', 'eeeee')
+# word = word.replace('ii', 'iiiii')
+# word = word.replace('oo', 'ooooo')
+# word = word.replace('uu', 'uuuuu')
+
+# print(word)
+
+# 6. Caesar Cipher
+# from pycipher import Caesar
+
+# answer = Caesar(key=-13).decipher('lbh zhfg hayrnea jung lbh unir yrnearq')
+# print(answer)
