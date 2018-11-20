a7a86a716d097c07a4932e68889b7fa4a1eee627
Oscar6 <oscar.m1024@gmail.com>
2018-11-20 08:59:04 -0600

    complete

---
+++ dictionariesExercises.py
@@ -0,0 +1,27 @@
+phonebook_dict = { 
+    #contact[0]
+        'Alice': '703-493-1834', 
+        'Bob': '857-384-1234', 
+        'Elizabeth': '484-584-2923'
+    }
+
+# 1. Print Elizabeth's phone number.
+# nameContact = phonebook_dict[0]['Elizabeth']
+# print(nameContact)
+
+
+# 2. Add a entry to the dictionary: Kareem's number is 938-489-1234.
+# phonebook_dict['Kareem'] = '484-584-2923'
+# print(phonebook_dict)
+
+# 3. Delete Alice's phone entry.
+# del phonebook_dict['Alice']
+# print(phonebook_dict)
+
+# 4. Change Bob's phone number to '968-345-2345'.
+# phonebook_dict['Bob'] = '968-345-2345'
+# print(phonebook_dict)
+
+
+# 5. Print all the phone entries.
+print(phonebook_dict)
