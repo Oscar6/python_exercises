30cae88edd0dca7fd6299586b8aca7a1b55f1c4f
Oscar6 <oscar.m1024@gmail.com>
2018-11-22 21:01:25 -0600

    update needed

---
+++ phoneBookApp.py
@@ -0,0 +1,10 @@
+# Phone Book App
+
+Exercise 1
+
+phonebook_dict = { 
+    #contact[0]
+        'Alice': '703-493-1834', 
+        'Bob': '857-384-1234', 
+        'Elizabeth': '484-584-2923'
+    }
