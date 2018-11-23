dcc54c162c38c5fa8c20538dc01bf30eebd72f47
Oscar6 <oscar.m1024@gmail.com>
2018-11-23 14:38:38 -0600

    menu completed

--- phoneBookApp.py
+++ phoneBookApp.py
@@ -1,10 +1,21 @@
 # Phone Book App
 
-Exercise 1
+def print_menu():       ## Your menu design here
+    print(25 * "=", "Phone Book App", 25 * "=")
+    print("1. Look up an entry")
+    print("2. Set an entry")
+    print("3. Delete an entry")
+    print("4. List all entries")
+    print("5. Quit")
 
-phonebook_dict = { 
-    #contact[0]
-        'Alice': '703-493-1834', 
-        'Bob': '857-384-1234', 
-        'Elizabeth': '484-584-2923'
-    }
+loop=True      
+  
+while loop:          ## While loop which will keep going until loop = False
+    print_menu()    ## Displays menu
+    choice = input("What do you want to do? Select [1-5]: ")
+     
+    if choice==1:     
+        print("Look up an entry. Please enter name: ")
+        ## You can add your code or functions here
+    elif choice==2:
+        print("Set an entry: ")
